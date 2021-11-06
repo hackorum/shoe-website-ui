5486564549dfe6122ea5fdb2d4dd0ffc4a850757
Neil Sabde <neilsabde@gmail.com>
2021-11-06 17:10:00 +0530

    something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.png";
+import shoe from "./shoe.png";
+import shoe2 from "./shoe2.png";
+import menu from "./menu.png";
 
 function App() {
+  const toggleClick = () => {
+    let list = document.querySelector(".two");
+    if (list.style.maxHeight == "0px") {
+      list.style.maxHeight = "300px";
+    } else {
+      list.style.maxHeight = "0px";
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <div className="one">
+          <img className="logo" src={logo} width="40px" />
+          <li>SUPER SHOES</li>
+        </div>
+        <div className="two">
+          <li>
+            <a href="index.html">HOME</a>
+          </li>
+          <li>
+            <a href="shop.html">SHOP</a>
+          </li>
+          <li>
+            <a href="contact.html">CONTACT</a>
+          </li>
+          <li>
+            <a href="about.html">ABOUT</a>
+          </li>
+        </div>
+        <img
+          src={menu}
+          width="30px"
+          className="menuicon"
+          onClick={toggleClick}
+        />
+      </ul>
+      <div className="left-container">
+        <div className="text">
+          <img src={shoe2} />
+          <h1>Super Shoes</h1>
+          <p>We've got the perfect shoes for you.</p>
+          <button className="btn">
+            <a href="shoes.html">SHOP NOW</a>
+          </button>
+        </div>
+        <img src={shoe} />
+      </div>
     </div>
   );
 }
